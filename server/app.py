5d29d7ad4ba3a6bc03096ea6e76f2fc53f2711c2
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-10-13 20:55:57 +0300

    i explained everything within the code

--- server/app.py
+++ server/app.py
@@ -2,8 +2,7 @@
 
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
-from flask_restful import Api, Resource
+from flask import Flask, request, make_response, jsonify
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -23,6 +22,81 @@ db.init_app(app)
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
+
+
+"""
+jsonify is used to covert python data structures like dictionaries into a JSON (javascript Object Notation ) response
+"""
+
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([restaurant.to_dict() for restaurant in restaurants]), 200
+# to_dict() is used to covert an object to a python dictionary 
+
+
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        return jsonify({
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address,
+            'restaurant_pizzas': [pizza.to_dict() for pizza in restaurant.restaurant_pizzas]
+        })
+    return jsonify({'error': 'Restaurant not found'}), 404
+
+# int:id is the placeholder URL for an integer value .This is how flask makes sure you are providing valid data
+
+
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant is None:
+        return make_response({"error": "Restaurant not found"}), 404
+    db.session.delete(restaurant)
+    db.session.commit()
+    return '', 204
+# 204 error means there is no content ,it is used to indicate that the request was successful but there was no content provided
+
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([{
+        'id': pizza.id,
+        'name': pizza.name,
+        'ingredients': pizza.ingredients
+    } for pizza in pizzas]), 200
+
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    if 'price' not in data or not (1 <= data['price'] <= 30):
+        return jsonify({'errors': ['Price must be between 1 and 30']}), 400
+
+    pizza = Pizza.query.get(data['pizza_id'])
+    restaurant = Restaurant.query.get(data['restaurant_id'])
+
+    if not pizza or not restaurant:
+        return jsonify({'errors': ['Pizza or Restaurant not found']}), 404
+
+    restaurant_pizza = RestaurantPizza(price=data['price'], pizza_id=pizza.id, restaurant_id=restaurant.id)
+    db.session.add(restaurant_pizza)  # used to add an object to the database
+    db.session.commit()  # finalizes  the changes to the database
+
+    return jsonify({
+        'id': restaurant_pizza.id,
+        'price': restaurant_pizza.price,
+        'pizza_id': restaurant_pizza.pizza_id,
+        'restaurant_id': restaurant_pizza.restaurant_id,
+        'pizza': {'id': pizza.id, 'name': pizza.name},
+        'restaurant': {'id': restaurant.id, 'name': restaurant.name}
+    }), 201
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -17,8 +17,17 @@ class Restaurant(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+        }
 
     # add relationship
+    # back_populates is somehow the same as backref but it give you more  control
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant')
 
     # add serialization rules
 
@@ -32,8 +41,17 @@ class Pizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
+    # to_dict () to convert an object to a dictionary
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "ingredients": self.ingredients,
+        }
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
 
     # add serialization rules
 
@@ -46,12 +64,22 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-
+    # adding  a foreign key to the restaurant table
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    # adding  a foreign key to the pizza table
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
     # add relationships
-
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    
     # add serialization rules
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if not (1 <= price <= 30):
+            raise ValueError("Price must be between 1 and 30.")
+        return price
 
     def __repr__(self):
         return f'<RestaurantPizza ${self.price}>'
